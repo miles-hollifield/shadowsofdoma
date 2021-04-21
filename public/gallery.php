fb4add05a276d546106d8e4270d0bd37e391984e
Miles <mileshollifieldgfp@gmail.com>
2021-04-21 12:35:24 -0400

    Fixed flex display of gallery and alignment

--- public/gallery.php
+++ public/gallery.php
@@ -17,21 +17,25 @@
 
         <!-- Thumbnail Gallery -->
         <div id="gallery-grid">
-          <img src="<?php echo url_for('/img/gallery/ff14-group-photo-large.jpg'); ?>" alt="Free Company Group Photo" width="345" height="194">
-          <img src="<?php echo url_for('/img/gallery/ff14-shadowbringers-large.jpg'); ?>" alt="Free Company Dungeon" width="345" height="194">
-          <img src="<?php echo url_for('/img/gallery/ff14-rival-wings-large.jpg'); ?>" alt="Free Company Fight" width="345" height="194">
+          <img src="<?php echo url_for('/img/gallery/ff14-group-photo-small.jpg'); ?>" alt="Free Company Group Photo" width="345" height="194">
+          <img src="<?php echo url_for('/img/gallery/ff14-shadowbringers-small.jpg'); ?>" alt="Free Company Dungeon" width="345" height="194">
+          <img src="<?php echo url_for('/img/gallery/ff14-rival-wings-small.jpg'); ?>" alt="Free Company Fight" width="345" height="194">
+          <img src="<?php echo url_for('/img/gallery/ff14-all-black-small.jpg'); ?>" alt="Free Company All Black Photo" width="345" height="194">
         </div>
 
         <!-- Modal -->
         <div id="modal">
           <div id="gallery-item-1">
-            <img src="img/gallery/ff14-group-photo-large.jpg" alt="Free Company Group Photo" width="1112" height="626">
+            <img src="<?php echo url_for('/img/gallery/ff14-group-photo-large.jpg'); ?>" alt="Free Company Group Photo" width="1112" height="626">
           </div>
           <div id="gallery-item-2">
-            <img src="img/gallery/ff14-shadowbringers-large.jpg" alt="Free Company Dungeon" width="1112" height="626">
+            <img src="<?php echo url_for('/img/gallery/ff14-shadowbringers-large.jpg'); ?>" alt="Free Company Dungeon" width="1112" height="626">
           </div>
           <div id="gallery-item-3">
-            <img src="img/gallery/ff14-rival-wings-large.jpg" alt="Free Company Fight" width="1112" height="626">
+            <img src="<?php echo url_for('/img/gallery/ff14-rival-wings-large.jpg'); ?>" alt="Free Company Fight" width="1112" height="626">
+          </div>
+          <div id="gallery-item-4">
+            <img src="<?php echo url_for('/img/gallery/ff14-all-black-large.jpg'); ?>" alt="Free Company All Black Photo" width="1112" height="626">
           </div>
         </div>
      
