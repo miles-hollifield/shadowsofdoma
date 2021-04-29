90ef71de29a49b54937be49f9e8949cb77a092d9
Miles <mileshollifieldgfp@gmail.com>
2021-04-28 21:14:34 -0400

    Fixed admin logout message

--- public/admin/logout.php
+++ public/admin/logout.php
@@ -2,9 +2,9 @@
 require_once('../../private/initialize.php');
 
 // Log out the admin
-$session->logout();
-
 $session->message('Logout successful. See you next time, ' . $session->user_name . '!');
+
+$session->logout();
 
 redirect_to(url_for('./index.php'));
 
