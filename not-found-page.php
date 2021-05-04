85d47e1311bddb2fd34988bac338689828a48a98
Miles <mileshollifieldgfp@gmail.com>
2021-05-04 09:43:25 -0400

    Fixed 404 page final

--- not-found-page.php
+++ not-found-page.php
@@ -9,8 +9,8 @@
   <head>
     <meta charset="utf-8">
     <title><?php if(isset($pageTitle)) { echo h($pageTitle) . ' - '; } ?>Shadows of Doma</title>
+    <link href="<?php echo url_for('../public/css/general.css'); ?>" rel="stylesheet">
     <link href="<?php echo url_for('../public/css/styles.css'); ?>" rel="stylesheet">
-    <link href="<?php echo url_for('../public/css/grid.css'); ?>" rel="stylesheet">
     <link href="<?php echo url_for('../public/css/modal.css'); ?>" rel="stylesheet">
     <script src='https://www.google.com/recaptcha/api.js'></script>
   </head>
