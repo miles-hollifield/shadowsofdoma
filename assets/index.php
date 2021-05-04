a53da19816225e2faf27b7154dd33e5f86d1adce
Miles <mileshollifieldgfp@gmail.com>
2021-05-04 07:52:37 -0400

    Finished assets index.php

--- assets/index.php
+++ assets/index.php
@@ -15,31 +15,6 @@
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   </head>
   <body>
-    <header>
-      <div id="masthead">
-        <div id="header-title">
-          <a href="index.php"><h1>SHADOWS OF DOMA</h1></a>
-          <a href="index.php"><p>FINAL FANTASY XIV</p></a>
-          <a href="index.php"><p>FREE COMPANY/GUILD WEBSITE</p></a>
-        </div>
-      </div>
-      <nav id="navbar">
-        <a class="<?php if($page == "home") { echo "current"; } ?>" href="index.php">Home</a>
-        <a class="<?php if($page == "about") { echo "current"; } ?>" href="about.php">About Us</a>
-        <a class="<?php if($page == "staff") { echo "current"; } ?>" href="staff.php">Meet Our Staff</a>
-        <a class="<?php if($page == "gallery") { echo "current"; } ?>" href="gallery.php">Gallery</a>
-        <a class="<?php if($page == "roster") { echo "current"; } ?>" href="roster.php">Roster</a>
-        <?php if($page !== '404') { ?>
-          <a class="<?php if($page == "login" || $page == "signup") { echo "current"; } ?>" href="login.php">Log in/Register</a>
-        <?php } ?>
-        <div class="search-container">
-          <form action="search.php" method="get">
-            <input type="text" placeholder="Search Character..." name="search">
-            <button type="submit" value="GO">GO</button>
-          </form>
-        </div>
-      </nav>
-    </header>
     <div id="wrapper">
 
       <?php echo display_session_message(); ?>
@@ -74,69 +49,72 @@
           <img src="ff14-roleplay.jpg" alt="Free Company Roleplay Photo" width="359" height="202"><br>
           <a href="https://twitter.com/iifetakers">https://twitter.com/iifetakers</a><br>
 
-            <!-- Start Here -->
+          <img src="ff14-casual.jpg" alt="Free Company Casual Photo" width="359" height="202"><br>
+          <a href="https://www.pcgamesn.com/final-fantasy-xiv-a-realm-reborn/ban-players">https://www.pcgamesn.com/final-fantasy-xiv-a-realm-reborn/ban-players</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="parchment-small.jpg" alt="Parchment" width="359" height="202"><br>
+          <a href="https://depositphotos.com/25466401/stock-photo-old-parchment-paper-background.html">https://depositphotos.com/25466401/stock-photo-old-parchment-paper-background.html</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="wooden-bg.jpg" alt="Wooden Background" width="359" height="202"><br>
+          <a href="https://www.wallpaperflare.com/wood-background-brown-wooden-surface-aero-patterns-wall-texture-wallpaper-pbmbl">https://www.wallpaperflare.com/wood-background-brown-wooden-surface-aero-patterns-wall-texture-wallpaper-pbmbl</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-stance-small.jpg" alt="Free Company Stance Photo" width="359" height="202"><br>
+          <a href="https://pinoygamer.ph/threads/final-fantasy-xiv-philippines-faq.5317/">https://pinoygamer.ph/threads/final-fantasy-xiv-philippines-faq.5317/</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-group-photo-small.jpg" alt="Free Company Group Photo" width="359" height="202"><br>
+          <a href="https://line.17qq.com/articles/ccccodhlpv.html">https://line.17qq.com/articles/ccccodhlpv.html</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-all-black-small.jpg" alt="Free Company All Black Photo" width="359" height="202"><br>
+          <a href="https://www.mmorpg.com/guides/sprouts-guide-to-glamour-and-cosmetics-in-final-fantasy-14-2000120838">https://www.mmorpg.com/guides/sprouts-guide-to-glamour-and-cosmetics-in-final-fantasy-14-2000120838</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-shadowbringers-small.jpg" alt="Free Company Shadowbringers Photo" width="359" height="202"><br>
+          <a href="https://www.playstationlifestyle.net/2020/02/03/final-fantasy-14-update-will-come-mid-february-adds-new-quests/#/slide/1">https://www.playstationlifestyle.net/2020/02/03/final-fantasy-14-update-will-come-mid-february-adds-new-quests/#/slide/1</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-ultima-boss-small.jpg" alt="Free Company Ultima Boss" width="359" height="202"><br>
+          <a href="https://square-enix-games.com/en_GB/news/final-fantasy-bosses">https://square-enix-games.com/en_GB/news/final-fantasy-bosses</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-ingame-small.jpg" alt="Free Company In-Game Photo" width="359" height="202"><br>
+          <a href="https://www.ign.com/wikis/ff14/Ala_Mhigo">https://www.ign.com/wikis/ff14/Ala_Mhigo</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-mech-boss-small.jpg" alt="Free Company Mech Boss Photo" width="359" height="202"><br>
+          <a href="https://gamerescape.com/2021/01/25/we-asked-ffxivs-naoki-yoshida-about-5-3-5-4-and-shirts/">https://gamerescape.com/2021/01/25/we-asked-ffxivs-naoki-yoshida-about-5-3-5-4-and-shirts/</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-rival-wings-small.jpg" alt="Free Company Beach Photo" width="359" height="202"><br>
+          <a href="https://www.reddit.com/r/ffxiv/comments/9ls9to/spreading_your_wings_an_indepth_rival_wings_guide/">https://www.reddit.com/r/ffxiv/comments/9ls9to/spreading_your_wings_an_indepth_rival_wings_guide/</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="gallery/ff14-armored-small.jpg" alt="Free Company Armored Photo" width="359" height="202"><br>
+          <a href="https://www.thegamer.com/square-enix-sony-making-live-action-final-fantasy-xiv-tv-show/ ">https://www.thegamer.com/square-enix-sony-making-live-action-final-fantasy-xiv-tv-show/ </a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
-
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-council-1.jpg" alt="Gloria Vidar" width="179" height="300"><br>
+          <a href="https://www.pinterest.com/RoyaIRaven/character-outfits/">https://www.pinterest.com/RoyaIRaven/character-outfits/</a><br>
           
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-council-2.jpg" alt="Marco Wrynn" width="234" height="300"><br>
+          <a href="https://www.pinterest.com/danniekennedy13/final-fantasy/">https://www.pinterest.com/danniekennedy13/final-fantasy/</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-council-3.jpg" alt="Lukas Reeves" width="179" height="300"><br>
+          <a href="https://ffxiv.eorzeacollection.com/glamour/33488/misfortune-caller-of-crows">https://ffxiv.eorzeacollection.com/glamour/33488/misfortune-caller-of-crows</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-leader.jpg" alt="Falcor Stormrage" width="300" height="300"><br>
+          <a href="https://www.reddit.com/r/FFXIVGlamours/comments/cgtvf4/drk_warrior_of_light_and_darkness/">https://www.reddit.com/r/FFXIVGlamours/comments/cgtvf4/drk_warrior_of_light_and_darkness/</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-officer-1.jpg" alt="Naga Fellrain" width="179" height="300"><br>
+          <a href="https://www.pinterest.de/pin/634655772463375379/">https://www.pinterest.de/pin/634655772463375379/</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-officer-2.jpg" alt="Alfred Galar" width="252" height="300"><br>
+          <a href="https://rowdyroegadyn.tumblr.com/post/162093900414/kazberks-a-true-blue-samurai-now-im-having-a/embed">https://rowdyroegadyn.tumblr.com/post/162093900414/kazberks-a-true-blue-samurai-now-im-having-a/embed</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-officer-3.jpg" alt="Aleidis Westmoore" width="179" height="300"><br>
+          <a href="https://www.pinterest.com/ceramicgurl/ffxiv/">https://www.pinterest.com/ceramicgurl/ffxiv/</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-officer-4.jpg" alt="Yuki Nawashiro" width="179" height="300"><br>
+          <a href="https://www.pinterest.com/raberr/ffxiv-glam-ideas/">https://www.pinterest.com/raberr/ffxiv-glam-ideas/</a><br>
 
-          <img src="ff14-beach-small.png" alt="Free Company Beach Photo" width="359" height="202"><br>
-          <a href="http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/">http://ffxivrealm.com/threads/chocobo-crossing-na-free-company-is-recruiting.3759/</a><br>
+          <img src="staff/ff14-officer-5.jpg" alt="Aang Jopino" width="179" height="300"><br>
+          <a href="https://ffxiv.eorzeacollection.com/glamour/23474/explorer">https://ffxiv.eorzeacollection.com/glamour/23474/explorer</a><br>
 
       </main>
+      <footer>
+        <p>&#169; Copyright 2021. Created by Miles Hollifield.</p>
+      </footer>
+  </body>
+</html>
